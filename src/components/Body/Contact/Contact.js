886c76ef9d22bc4871c4f55e7601234f70aef73a
epalma <epalma@EPALMA-LAP.oradev.oraclecorp.com>
2022-10-17 21:37:13 -0500

    email and phone number validation

--- src/components/Body/Contact/Contact.js
+++ src/components/Body/Contact/Contact.js
@@ -8,6 +8,8 @@ import Snackbar from '@mui/material/Snackbar';
 import { ThemeProvider } from '@mui/material/styles';
 import { useTranslation } from 'react-i18next';
 import { useRef, useState, SyntheticEvent} from 'react';
+import handlePhone from '../../../utilities/validatePhone';
+import handleEmail from '../../../utilities/validateEmail';
 
 import logo from '../../../images/logo.png';
 
@@ -20,24 +22,38 @@ const Contact = ({theme}) => {
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState(false);
     const [failure, setFailure] = useState(false);
+    const [warning, setWarning] = useState(false)
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
 
     const submitEmail = (e) => {
+        
         e.preventDefault();
         setLoading(true);
 
-        emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current, process.env.REACT_APP_EMAILJS_USER_ID)
-        .then((result) => {
+        console.log(handleEmail(email));
+        console.log(handlePhone(phone));
+
+        if(handleEmail(email) && handlePhone(phone)){
+            emailjs.sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID, form.current, process.env.REACT_APP_EMAILJS_USER_ID)
+            .then((result) => {
+                setLoading(false);
+                setSuccess(true);
+                console.log(result.text);
+            }, 
+            (error) => {
+                setLoading(false);
+                setFailure(true);
+                console.log(error.text);
+                
+            });
+        }
+        else{
             setLoading(false);
-            setSuccess(true);
-            console.log(result.text);
-        }, 
-        (error) => {
-            alert("JAJAJAJA");
-            setLoading(false);
-            setFailure(true);
-            console.log(error.text);
-            
-        });
+            setWarning(true);
+        }
+
+
     }
 
     const handleClose = (event: SyntheticEvent | Event, reason?: string) => {
@@ -47,6 +63,7 @@ const Contact = ({theme}) => {
         
         setSuccess(false);
         setFailure(false);
+        setWarning(false);
     }
 
     return (
@@ -71,10 +88,10 @@ const Contact = ({theme}) => {
                             <TextField type="text" color="secondary" name="user_name" label="Name" variant="outlined" className="field-size" required/>
                         </Grid>
                         <Grid xs={12} className="margin-size">
-                            <TextField type="text" color="secondary" name="user_email" label="Email" variant="outlined" className="field-size" required/>
+                            <TextField type="text" color="secondary" onChange={event => setEmail(event.target.value)} name="user_email" label="Email" variant="outlined" className="field-size" required/>
                         </Grid>
                         <Grid xs={12} className="margin-size">
-                            <TextField type="text" color="secondary" name="user_phone" label="Phone Number" variant="outlined" className="field-size"/>
+                            <TextField type="text" color="secondary" onChange={event => setPhone(event.target.value)} name="user_phone" label="Phone Number" variant="outlined" className="field-size"/>
                         </Grid>
                         <Grid xs={12} className="margin-size">
                             <TextField type="text" color="secondary" name="message" label="Message" multiline rows={10} className="field-size" required/>
@@ -102,6 +119,15 @@ const Contact = ({theme}) => {
                 <Snackbar open={failure} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: 'botton', horizontal: 'center'}}>
                     <Alert onClose={handleClose} severity="error">
                         Ops! something went wrong. <strong>Try it again in few minutes.</strong>
+                    </Alert>
+                </Snackbar>
+            </div>
+
+            
+            <div>
+                <Snackbar open={warning} autoHideDuration={3000} onClose={handleClose} anchorOrigin={{ vertical: 'botton', horizontal: 'center'}}>
+                    <Alert onClose={handleClose} severity="warning">
+                        { !handleEmail(email)? "Your email is not valid. Enter a valid email." : "Your Phone Number is not valid. Enter a valid number"}
                     </Alert>
                 </Snackbar>
             </div>
