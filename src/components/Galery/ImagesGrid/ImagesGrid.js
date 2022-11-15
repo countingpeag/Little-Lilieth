81ff1e1f5d3fd645b5370a0e969ed72859babab8
epalma <epalma@EPALMA-LAP.oradev.oraclecorp.com>
2022-11-14 20:42:46 -0600

    new modal for each product

--- src/components/Galery/ImagesGrid/ImagesGrid.js
+++ src/components/Galery/ImagesGrid/ImagesGrid.js
@@ -8,11 +8,41 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CardActionArea from '@mui/material/CardActionArea';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 import '../../../Styles/galery.css';
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  
+
 const ImagesGrid = ({title, images}) => {
 
+    const [open, setOpen] = useState(false);
+    const [currentImage, setCurrentImage] = useState("");
+    const [currentTitle, setCurrentTitle] = useState("");
+    const [currentDescription, setcurrentDescription] = useState("");
+
+    const handleOpen = (item) => {
+        setCurrentImage(item.image);
+        setCurrentTitle(item.title);
+        setcurrentDescription(item.description);
+        setOpen(true);
+    }
+
+    const handleClose = () => setOpen(false);
+  
     const matches = useMediaQuery('(min-width:600px)');
 
     return(
@@ -27,9 +57,8 @@ const ImagesGrid = ({title, images}) => {
                             <ImageListItem key={item.title}>
                                 <Card sx={{ maxWidth: 345 }}>
                                     
-                                    <CardActionArea>
-                                    <CardMedia component="img" height={matches ? 400 : 200} image={item.image} alt={item.title}/>
-                                    
+                                    <CardActionArea onClick={() => handleOpen(item)}>
+                                        <CardMedia component="img" height={matches ? 400 : 200} image={item.image} alt={item.title}/>
                                     </CardActionArea>
                                     <CardContent>
                                         <Typography variant="subtitle1" color="text.secondary" component="div">
@@ -48,6 +77,18 @@ const ImagesGrid = ({title, images}) => {
                     }
                 </ImageList>
             </Grid>
+
+            <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        {currentTitle}
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {currentDescription}
+                    </Typography>
+                    <Button onClick={handleClose}>Add</Button>
+                </Box>
+            </Modal>
         </Grid>
     );
 }
