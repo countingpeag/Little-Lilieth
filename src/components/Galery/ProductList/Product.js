de7078a979414caa6da4446dc0eccb83d72320c2
epalma <epalma@EPALMA-LAP.oradev.oraclecorp.com>
2022-11-23 21:16:10 -0600

    sizing components

--- src/components/Galery/ProductList/Product.js
+++ src/components/Galery/ProductList/Product.js
@@ -8,55 +8,54 @@ import RemoveIcon from '@mui/icons-material/Remove';
 import Modal from '@mui/material/Modal';
 import IconButton from '@mui/material/IconButton';
 
+import '../../../Styles/product.css';
+
 const Product = ({theme, opened, closeMethod, addMethod, removeMethod, matches, image, amount, price}) => {
 
-    const boxStyle = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '80%',
-        height: '80%',
-        bgcolor: 'background.paper',
-        border: '2px solid #000',
-        boxShadow: 24,
-        p: 4,
-    };
+
 
     return(
         <Modal open={opened} onClose={closeMethod} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-            <Box sx={boxStyle}>
-                <Grid container>
-                    <Grid xs={6}>
+            <Box className="box">
+                <Grid container className="container">
+                    <Grid xs={6} className="color2">
                         <CardMedia component="img" height={matches ? 400 : 200} image={image} alt={"image"}/>
                     </Grid>
-                    <Grid xs={6}>
-                        <Typography id="modal-modal-title" variant="h4" component="h4">
-                            TITLE
-                        </Typography>
-                        <Typography id="modal-modal-title" variant="h6" component="h6">
-                            Sub Title
-                        </Typography>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                            <ul>
-                                <li>This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.</li>
-                                <li>This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.</li>
-                                <li>This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.</li>
-                            </ul>
-                        </Typography>
-
-                        <IconButton aria-label="previous" onClick={removeMethod}>
-                            <RemoveIcon/>
-                        </IconButton>
-                            {amount}
-                        <IconButton aria-label="next" onClick={addMethod}>
-                            <AddIcon/>
-                        </IconButton>
-                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                            {`Price:${price}`}
-                        </Typography>
-                        <Button theme={theme} variant="contained" color="secondary" onClick={closeMethod}>Add to Cart</Button>
-                        <Button theme={theme} variant="outlined" color="secondary" onClick={closeMethod}>Cancel</Button>
+                    <Grid xs={6} className="color1">
+                        <Grid xs={12}>
+                            <Typography id="modal-modal-title" variant="h4" component="h4">
+                                TITLE
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12}>
+                            <Typography id="modal-modal-title" variant="h6" component="h6">
+                                Sub Title
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12} container justifyContent="center">
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }} className="descriptionSize"> 
+                                This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.
+                                This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.
+                                This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12}>
+                            <IconButton aria-label="previous" onClick={removeMethod}>
+                                <RemoveIcon/>
+                            </IconButton>
+                                {amount}
+                            <IconButton aria-label="next" onClick={addMethod}>
+                                <AddIcon/>
+                            </IconButton>
+                        </Grid>
+                        <Grid xs={12}>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                {`Price:${price}`}
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12}>
+                            <Button theme={theme} variant="contained" color="secondary" onClick={closeMethod} className="buttonSize">Add to Cart</Button>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Box>
