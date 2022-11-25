676545792de5d62f267e93601ba8e54e3c285311
epalma <epalma@EPALMA-LAP.oradev.oraclecorp.com>
2022-11-24 21:15:33 -0600

    position refactoring

--- src/components/Galery/ProductList/Product.js
+++ src/components/Galery/ProductList/Product.js
@@ -7,53 +7,54 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Modal from '@mui/material/Modal';
 import IconButton from '@mui/material/IconButton';
+import Card from '@mui/material/Card';
 
 import '../../../Styles/product.css';
 
 const Product = ({theme, opened, closeMethod, addMethod, removeMethod, matches, image, amount, price}) => {
 
-
-
     return(
         <Modal open={opened} onClose={closeMethod} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
             <Box className="box">
                 <Grid container className="container">
-                    <Grid xs={6} className="color2">
-                        <CardMedia component="img" height={matches ? 400 : 200} image={image} alt={"image"}/>
+                    <Grid xs={6} className="imageSpace">
+                            <Card sx={{ maxWidth: 450 }}>
+                                <CardMedia component="img" height={matches ? 400 : 200} image={image} alt={"image"}/>
+                            </Card>
                     </Grid>
                     <Grid xs={6} className="color1">
                         <Grid xs={12}>
-                            <Typography id="modal-modal-title" variant="h4" component="h4">
+                            <Typography id="modal-modal-title" variant="h4" component="h4" align='left'>
                                 TITLE
                             </Typography>
                         </Grid>
                         <Grid xs={12}>
-                            <Typography id="modal-modal-title" variant="h6" component="h6">
+                            <Typography id="modal-modal-title" variant="h6" component="h6" align='left'>
                                 Sub Title
                             </Typography>
                         </Grid>
-                        <Grid xs={12} container justifyContent="center">
-                            <Typography id="modal-modal-description" sx={{ mt: 2 }} className="descriptionSize"> 
-                                This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.
-                                This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.
+                        <Grid xs={12}>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }} className="descriptionSize" align='left'> 
                                 This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties. This is a perfect bow to use in your christmas parties.
                             </Typography>
                         </Grid>
-                        <Grid xs={12}>
+                        <Grid xs={12} container justifyContent="left">
                             <IconButton aria-label="previous" onClick={removeMethod}>
                                 <RemoveIcon/>
                             </IconButton>
+                            <IconButton aria-label="previous">
                                 {amount}
+                            </IconButton>
                             <IconButton aria-label="next" onClick={addMethod}>
                                 <AddIcon/>
                             </IconButton>
                         </Grid>
                         <Grid xs={12}>
-                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }} align="left">
                                 {`Price:${price}`}
                             </Typography>
                         </Grid>
-                        <Grid xs={12}>
+                        <Grid xs={12} container justifyContent="left">
                             <Button theme={theme} variant="contained" color="secondary" onClick={closeMethod} className="buttonSize">Add to Cart</Button>
                         </Grid>
                     </Grid>
