af28505f4be1b2654a4e9c02c21b756d3e3eafbf
epalma <epalma@EPALMA-LAP.oradev.oraclecorp.com>
2022-12-01 20:55:45 -0600

    shopingkart component

---
+++ src/components/ShopingKart/ShopingKart.js
@@ -0,0 +1,11 @@
+import Grid from '@mui/material/Unstable_Grid2';
+
+const ShopingKart = () => {
+    return (
+        <Grid>
+            <h1>Shoping Kart</h1>
+        </Grid>
+    );
+}
+
+export default ShopingKart;
