ca7e68f703844e213497e638878026aa72b00094
Erick Palma <erick.palma@oracle.com>
2023-04-18 21:31:47 -0600

    new quantity editor for shoppingCart elements

--- src/components/ShoppingCart/ShoppingElements.js
+++ src/components/ShoppingCart/ShoppingElements.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Grid from '@mui/material/Unstable_Grid2';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
@@ -6,12 +7,25 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import CardActionArea from '@mui/material/CardActionArea';
 import Button from '@mui/material/Button';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 import '../../Styles/shoppingCart.css';
 
 const ShoppingElements = ({images}) => {
 
+    const [quantity, setquantity ] = useState(0);
     const matches = useMediaQuery('(min-width:600px)');
+
+    const handleReduce = () => {
+        if(quantity>0)
+            setquantity(quantity-1);
+    }
+
+    const handleAdd = () => {
+        setquantity(quantity+1);
+    }
 
     return (
         <Grid Container>
@@ -31,7 +45,15 @@ const ShoppingElements = ({images}) => {
                                     <Grid xs={5} className="description">
                                         <h2>{item.title}</h2>
                                         <p>{item.description}</p>
-                                        Quantity: 1  <Button variant="text"><DeleteOutlineIcon /></Button>  
+                                        Quantity: 
+                                        <IconButton aria-label="previous" onClick={handleReduce}>
+                                            <RemoveIcon/>
+                                        </IconButton>
+                                        {quantity}
+                                        <IconButton aria-label="next" onClick={handleAdd}>
+                                            <AddIcon/>
+                                        </IconButton>  
+                                        <Button variant="text"><DeleteOutlineIcon /></Button>  
                                     </Grid>
                                     <Grid xs={4} className="price">
                                         <h2>Price</h2>
